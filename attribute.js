fb2d0926e43cbabeae33261f5ba32261c05f7156
shahinexy <shahinexy@gmail.com>
2024-02-10 19:35:21 +0600

    learn about classList

--- attribute.js
+++ attribute.js
@@ -1,4 +1,7 @@
+console.log('======================');
+
 // Attribute 
 // by getElementById you will get all of the class name
-const attribute = document.getElementById('attribute');
+const attribute = document.getElementById('dom_attribute');
 console.log(attribute.getAttribute('class'));
+console.log(attribute.getAttribute('id'));
---
+++ classList.js
@@ -0,0 +1,8 @@
+console.log('========================');
+const listClass = document.getElementById('dom_classlist');
+console.log(listClass.classList);
+listClass.classList.remove('title')
+console.log(listClass.classList);
+listClass.classList.add('new_class')
+console.log(listClass.classList);
+
